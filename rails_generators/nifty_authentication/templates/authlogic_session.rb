0b74ac073ee99c5200de6c0f0a54c2e7fa5eecb3
niklas <niklas@brueckenschlaeger.de>
2010-09-26 07:09:13 +0200

    workaround for authlogic bug

--- rails_generators/nifty_authentication/templates/authlogic_session.rb
+++ rails_generators/nifty_authentication/templates/authlogic_session.rb
@@ -1,2 +1,6 @@
 class <%= session_class_name %> < Authlogic::Session::Base
+   # Workaround for http://github.com/binarylogic/authlogic/issues/issue/101
+   def to_key
+     new_record? ? nil : [ self.send(self.class.primary_key) ]
+   end
 end
